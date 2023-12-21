d469cb2ca3d8650f6acf0dca57c157cee2f2b5f4
Carson Haskell <carson.haskell@gmail.com>
2023-12-21 10:16:15 -0800

    upload initial project

---
+++ src/components/CategoryItem/CategoryItem.js
@@ -0,0 +1,22 @@
+import "./CategoryItem.styles.scss";
+
+function CategoryItem({ category }) {
+  const { id, title, imageUrl } = category;
+
+  return (
+    <div key={id} className="category-container">
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="category-body-container">
+        <h2>{title}</h2>
+        <p>Shop now</p>
+      </div>
+    </div>
+  );
+}
+
+export default CategoryItem;
